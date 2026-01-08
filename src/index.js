b60b4c55f2fd96948b718d0fd4f69523b0793f42
Dhruv Singh <dhruvsingh7077@gmail.com>
2026-01-08 15:58:02 +0530

    Restructure src: add controllers/middlewares/routes/services/utils, update config/index.js, remove old controllers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
-const { PORT } = require("./config");
-
+const { ServerConfig, Logger } = require("./config");
+const apiRoutes = require("./routes");
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Server is running on port ${ServerConfig.PORT}`);
+  // Logger.info("Successfully started the server", "root", {});
 });
